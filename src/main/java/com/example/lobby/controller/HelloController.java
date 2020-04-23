b7a27a844f481b92dab31b7c2e097df8a8756317
grnstrn <starostingrigory@gmail.com>
2020-04-24 02:14:39 +0300

    divide frontend on modules with webpack

--- src/main/java/com/example/lobby/controller/HelloController.java
+++ src/main/java/com/example/lobby/controller/HelloController.java
@@ -1,6 +1,9 @@
 package com.example.lobby.controller;
 
 import com.example.lobby.domain.User;
+import com.example.lobby.repo.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,12 +15,23 @@ import java.util.HashMap;
 @Controller
 @RequestMapping("/")
 public class HelloController {
+    private final MessageRepo messageRepo;
+
+    @Value("${spring.profiles.active}")
+    private String profile;
+
+    @Autowired
+    public HelloController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
+
     @GetMapping
     public String hello(Model model, @AuthenticationPrincipal User user){
         HashMap<Object,Object> data = new HashMap<>();
         data.put("profile",user);
-        data.put("messages",null);
+        data.put("messages",messageRepo.findAll());
         model.addAttribute("frontendData",data);
+        model.addAttribute("isDevMode","dev".equals(profile));
         return "index";
     }
 }
